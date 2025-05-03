38f2d7adebbe4011113da146cc6da3d18bc431e4
Abhinav Kumar <abhinav.kumar@tigeranalytics.local>
2025-05-03 16:52:22 +0530

    removed azure key

--- app.py
+++ app.py
@@ -1,24 +1,3 @@
-# from flask import Flask, request, jsonify
-# from azure.storage.blob import BlobServiceClient
-# import os
-
-# app = Flask(__name__)
-
-# connect_str = 'DefaultEndpointsProtocol=https;AccountName=abhi1storage;AccountKey=IZuOEWTJz43scAc2KkBKmJn3OV5gB8ziCmjS4LxIjHYTsu9qvkRoqlvJXaSNb7IyUGMHWU25wZE3+AStI2X5hw==;EndpointSuffix=core.windows.net'
-# container_name = "azure-assignment"
-# blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-
-# @app.route('/list-files')
-# def list_files():
-#     folder = request.args.get('folder')
-#     blob_list = blob_service_client.get_container_client(container_name).list_blobs(name_starts_with=folder + "/")
-#     files = [blob.name for blob in blob_list]
-#     return jsonify(files)
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0')
-
-
 from flask import Flask, request, jsonify
 from azure.storage.blob import BlobServiceClient
 import os
